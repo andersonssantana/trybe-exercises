fd11cd4c2a51bc044900816ecab9f0431f4e4c64
Anderson Santana <anderslash@gmail.com>
2020-08-28 19:24:56 -0300

    Resolvido o exercício 7

--- Introduction/Bloco_4/Dia_2/exercicio07.js
+++ Introduction/Bloco_4/Dia_2/exercicio07.js
@@ -1,8 +1,11 @@
 // Utilizando for, descubra qual o menor valor contido no array e imprima-o;
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let menorNumero = numbers[0];
 
 for (let contador = 0; contador < numbers.length; contador += 1) {
-    for (let numeroAtual =0);
-
-console.log(numbers);
+    if (numbers[contador] <= menorNumero) {
+        menorNumero = numbers[contador];
+    }
+}
+console.log(menorNumero);
