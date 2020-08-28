93ddc417afb74a0b598b794c14570249b31a6b9c
Anderson Santana <anderslash@gmail.com>
2020-08-28 19:22:14 -0300

    Resolvido o exercício 5

--- Introduction/Bloco_4/Dia_2/exercicio05.js
+++ Introduction/Bloco_4/Dia_2/exercicio05.js
@@ -1,7 +1,11 @@
 // Utilizando for, descubra qual o maior valor contido no array e imprima-o;
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let maiorNumero = 0;
 
-numbers.sort();
-
-console.log(numbers);
+for (let contador = 0; contador < numbers.length; contador += 1) {
+    if (numbers[contador] >= maiorNumero) {
+        maiorNumero = numbers[contador];
+    }
+}
+console.log(maiorNumero);
--- Introduction/Bloco_4/Dia_2/exercicio07.js
+++ Introduction/Bloco_4/Dia_2/exercicio07.js
@@ -2,3 +2,7 @@
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
+for (let contador = 0; contador < numbers.length; contador += 1) {
+    for (let numeroAtual =0);
+
+console.log(numbers);
