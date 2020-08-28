bd6e7cae84ecf92542b43e894da2e02ba7bdc747
Anderson Santana <anderslash@gmail.com>
2020-08-28 18:12:35 -0300

    Inclusão do exercício 4

---
+++ Introduction/Bloco_4/Dia_2/exercicio04.js
@@ -0,0 +1,13 @@
+// Com o mesmo código do exercício anterior, caso valor final seja maior que 20, imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20";
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+    let soma = 0;
+    
+    for (let contador = 0; contador < numbers.length; contador += 1) {
+        soma = soma + numbers[contador];
+    }
+    if ((soma / numbers.length) > 20) {
+        console.log("valor maior que 20")
+    } else {
+        console.log("valor menor ou igual a 20")
+    }
