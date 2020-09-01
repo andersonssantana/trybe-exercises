b35cbf05884c020d3dc0d5c318b47b5afb56ac14
Anderson Santana <anderslash@gmail.com>
2020-09-01 18:20:05 -0300

    Inclusão do exercício 01

---
+++ Introduction/Bloco_4/Dia_4/refatorando4_1.js
@@ -0,0 +1,39 @@
+/* Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas variáveis, a e b, definidas no começo com os valores que serão operados. Faça programas para:
+Adição
+Subtracão
+Multiplicação
+Divisão
+Módulo */
+
+let a = 777;
+let b = 23;
+
+function soma(p1, p2) {
+    console.log(p1 + p2);
+}
+
+function subtracao(p1, p2) {
+    console.log(p1 - p2);
+}
+
+function multiplicacao(p1, p2) {
+    console.log(p1 * p2);
+}
+
+function divisao(p1, p2) {
+    console.log(p1 / p2);
+}
+
+soma(a, b);
+subtracao(a, b);
+multiplicacao(a, b);
+divisao(a, b);
+
+
+/* 
+console.log("O resultado de A + B é " + (a + b));
+console.log("O resultado de A - B é " + (a - b));
+console.log("O resultado de A * B é " + (a * b));
+console.log("O resultado de A / B é " + (a / b));
+console.log("O resultado de A % B é " + (a % b));
+*/
